bf3d4fd028747a99886c6867414d437e2aaf8c9c
willi <willi@10.0.0.87>
2021-09-16 11:31:06 -0600

    Added 1 line of code Made Hello world print on console

--- src/hello/world/HelloWorld.java
+++ src/hello/world/HelloWorld.java
@@ -15,7 +15,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String text = "Howdy";
+        String text = "Hello World";
+        System.out.println(text);
     }
     
 }
